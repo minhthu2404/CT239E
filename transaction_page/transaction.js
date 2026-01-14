0af3ff5a374e277223db16983e8fef5ca1eab140
kimins1202 <nguyenkimyen120205@gmail.com>
2026-01-14 16:08:42 +0700

    thêm giao dịch

---
+++ transaction_page/transaction.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById("addBtn");
+const cancel = document.getElementById("cancelBtn");
+const overlay = document.getElementById("overlay");
+const formPopup = document.getElementById("formPopup");
+
+btn.onclick = function () {
+    overlay.classList.remove("hidden");
+    formPopup.classList.remove("hidden");
+};
+
+// bấm nút Hủy
+cancel.onclick = function () {
+    closeForm();
+};
+
+// bấm ra ngoài (overlay)
+overlay.onclick = function () {
+    closeForm();
+};
+
+// chặn click trong form
+formPopup.onclick = function (e) {
+    e.stopPropagation();
+};
+
+function closeForm() {
+    overlay.classList.add("hidden");
+    formPopup.classList.add("hidden");
+}
