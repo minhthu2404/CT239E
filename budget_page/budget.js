48a46a5a6a1b8d1dd40af7b6667aa9404ce48655
Trần Minh Thư <thub2303851@student.ctu.edu.vn>
2026-03-05 21:30:49 +0700

    2

--- budget_page/budget.js
+++ budget_page/budget.js
@@ -1,11 +1,12 @@
 const btn_add = document.getElementsByClassName("setup");
-const btn_adjust= document.getElementsByClassName("adjust");
+const btn_adjust = document.getElementsByClassName("adjust");
 const overlay = document.getElementById("overlay");
 const formPopup = document.getElementById("formPopup");
 const title = document.getElementById("form-title");
 const cancel = document.getElementById("cancelBtn");
 
-const categoryName = document.getElementById("name-category");
+const categoryIcon = document.getElementById("form-category-icon");
+const categoryText = document.getElementById("form-category-text");
 
 // mở form
 for (let i = 0; i < btn_add.length; i++) {
@@ -14,28 +15,32 @@ for (let i = 0; i < btn_add.length; i++) {
         title.innerText = "Thiết lập ngân sách";
 
         const category = this.dataset.category;
-        categoryName.innerText = category;
+        const iconClass = this.closest('.budget').querySelector('.budget-title i').className;
+        categoryText.innerText = category;
+        categoryIcon.className = iconClass;
     });
 }
 
 //mở form chỉnh sửa
-for (let i=0; i < btn_adjust.length; i++){
-    btn_adjust[i].addEventListener("click", function(){
+for (let i = 0; i < btn_adjust.length; i++) {
+    btn_adjust[i].addEventListener("click", function () {
         overlay.classList.remove("hidden");
         title.innerText = "Chỉnh sửa thiết lập";
 
         const category = this.dataset.category;
-        categoryName.innerText = category;
+        const iconClass = this.closest('.budget').querySelector('.budget-title i').className;
+        categoryText.innerText = category;
+        categoryIcon.className = iconClass;
     })
 }
 
-function closeForm(){
+function closeForm() {
     overlay.classList.add("hidden");
 }
 
 //đóng form
-cancel.addEventListener("click",closeForm);
-overlay.addEventListener("click",closeForm);
+cancel.addEventListener("click", closeForm);
+overlay.addEventListener("click", closeForm);
 
 formPopup.addEventListener("click", function (e) {
     e.stopPropagation();
